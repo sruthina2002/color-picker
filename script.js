097f8d6cc58fd2ffb23a9dc932f59c3f89f2b74e
sruthina2002 <89062755+sruthina2002@users.noreply.github.com>
2021-08-17 18:49:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function makered(){
+    document.body.style.backgroundColor="red"; 
+}
+function makeblue(){
+    document.body.style.backgroundColor="blue"; 
+}
+function makegreen(){
+    document.body.style.backgroundColor="green"; 
+  }
+   function makeorange(){
+        document.body.style.backgroundColor="orange"; 
+   }
